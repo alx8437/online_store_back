92648687710f8462d6ba51a6f1f6952e059fae58
alx8437 <dezion777@gmail.com>
2021-07-01 00:05:52 +0300

    Added Error handler class

---
+++ error/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+    constructor(status, message) {
+        super();
+        this.status = status
+        this.message = message
+    }
+    /* Статические функции - это такие функции, которые можно вызывать без создания объекта
+    Можем напрямую обращаться напрямую к классу и возвращать ту или иную функцию */
+
+    static badRequest(message) {
+        return new ApiError(404, message)
+    }
+
+    static internal(message) {
+        return new ApiError(500, message)
+    }
+
+    static forbidden(message) {
+        return new ApiError(403, message)
+    }
+}
